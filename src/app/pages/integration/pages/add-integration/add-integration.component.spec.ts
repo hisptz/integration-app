1686dc39f1f0c04468e3e92d5ad93a53ca758b4b
Bennett <41388684+BaharaJr@users.noreply.github.com>
2020-01-29 23:58:18 +0300

    feat: add new component to implement adding new integration

---
+++ src/app/pages/integration/pages/add-integration/add-integration.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddIntegrationComponent } from './add-integration.component';
+
+describe('AddIntegrationComponent', () => {
+  let component: AddIntegrationComponent;
+  let fixture: ComponentFixture<AddIntegrationComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AddIntegrationComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddIntegrationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/integration/pages/add-integration/add-integration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-integration',
+  templateUrl: './add-integration.component.html',
+  styleUrls: ['./add-integration.component.css']
+})
+export class AddIntegrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
